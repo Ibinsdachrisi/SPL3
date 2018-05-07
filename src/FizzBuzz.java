707953694519c3aed99c2308dd68f80d9b8deb9e
Ibinsdachrisi <171wita31@o365.htl-leoben.at>
2018-05-07 14:34:17 +0200

    FizzBuzz, Version 3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,16 +3,20 @@
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            FizzerBuzzer(i);
+            FizzerBuzzer(i , true);
+        }
+        System.out.println("_______________");
+        for (int i = 0; i < 100; i += 4) {
+            FizzerBuzzer(i , false);
         }
     }
-    public static void FizzerBuzzer(int i){
-        if(i % 4 == 0 && i % 7 == 0){
-            System.out.println("FizzerBuzzer");
+    public static void FizzerBuzzer(int i, boolean showFB){
+        if(i % 4 == 0 && i % 7 == 0 && showFB == true){
+            System.out.println("FizzerBuzzer(" + i +")");
         }else if (i % 4 == 0) {
-            System.out.println("Fizzer");
+            System.out.println("Fizzer (" + i +")");
         }else if (i % 7 == 0){
-            System.out.println("Buzzer");
+            System.out.println("Buzzer (" + i +")");
         }else{
             System.out.println(i);
         }
