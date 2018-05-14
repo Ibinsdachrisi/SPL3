e4f75f86313a5ac264d83c608161835a5078e164
Ibinsdachrisi <171wita31@o365.htl-leoben.at>
2018-05-14 08:58:39 +0200

    WoerterRaten.java is ready to use

--- src/WoerterRaten.java
+++ src/WoerterRaten.java
@@ -2,30 +2,41 @@ import javax.swing.*;
 
 public class WoerterRaten {
     public static void main(String[] args) {
-        String zuerraten = JOptionPane.showInputDialog("Das zu erratende Wort eingeben");
-        char[] ZuerratenC = zuerraten.toCharArray();
-        char[] output = new char[ZuerratenC.length];
+        boolean wid = true;
+        while (wid == true) {
+            wid = false;
+            String zuerraten = JOptionPane.showInputDialog("Das zu erratende Wort eingeben");
+            char[] ZuerratenC = zuerraten.toCharArray();
+            char[] output = new char[ZuerratenC.length];
 
-        for (int i = 0; i < output.length; i++) {
-            output[i] = '*';
-        }
-        boolean erraten = false;
+            for (int i = 0; i < output.length; i++) {
+                output[i] = '*';
+            }
+            boolean erraten = false;
+            int versuche = 0;
 
-        while(erraten == false){
-            outputarr(output);
-            char[] input = JOptionPane.showInputDialog("Bitte das Wort Erraten!").toCharArray();
+            while (erraten == false) {
+                versuche++;
+                outputarr(output);
+                char[] input = JOptionPane.showInputDialog("Bitte das Wort Erraten!").toCharArray();
 
-            for (int i = 0; i < ZuerratenC.length; i++) {
-                for (int j = 0; j < input.length; j++) {
-                    if(ZuerratenC[i] == input[j]){
-                        output[i] = input[j];
+                for (int i = 0; i < ZuerratenC.length; i++) {
+                    for (int j = 0; j < input.length; j++) {
+                        if (ZuerratenC[i] == input[j]) {
+                            output[i] = input[j];
+                        }
                     }
                 }
+                erraten = fertig(output, ZuerratenC);
             }
-            
+            outputarr(output);
+            JOptionPane.showMessageDialog(null,"Das Wort <" + zuerraten + "> wurde nach " + versuche + ". Versuchen erraten ");
+            int inputms = JOptionPane.showConfirmDialog(null, "Wiederholen?");
+
+            if(inputms == 0){
+                wid = true;
+            }
         }
-
-
     }
 
     //
@@ -36,4 +47,18 @@ public class WoerterRaten {
         }
         System.out.println();
     }
+
+    public static boolean fertig(char output [], char input []) {
+        int count = 0;
+        for (int i = 0; i < output.length; i++) {
+            if (output[i] == input[i]){
+                count ++;
+            }
+        }
+        if(count == output.length){
+            return true;
+        }
+        return false;
+    }
 }
+//by christopher
